797eb87c32fabb96c211337c994a62e87eefbdf4
Frank Chen <jianwen.chen@uamaker.nyc>
2019-01-09 12:39:29 -0500

    Created module for data

---
+++ quizFC_data.py
@@ -0,0 +1,73 @@
+q0 = str("""What are the two parts of the U.S. Congress?
+    a. Senate, House of Representatives
+    b. FBI, CIA
+    c. President, Supreme Court
+    d. Department of Justice, Department of Education\n""")
+q1 = str("""The House of Representatives has how many voting members?
+    a. 12
+    b. 100
+    c. 9
+    d. 435\n""")
+q2 = str("""What does the judicial branch do?
+    a. Federal law enforcement
+    b. Write and pass new legislation
+    c. Authorize military action
+    d. Decide the Constitutionality of laws\n""")
+q3 = str("""When must all men register for the Selective Service?
+    a. At age 18
+    b. At age 21
+    c. At age 16
+    d. At age 55\n""")
+q4 = str("""What territory did the United States buy from France in 1803?
+    a. The Northwest Territory
+    b. The Louisiana Territory
+    c. Canada
+    d. Alaska\n""")
+q5 = str("""Name the U.S. war between the North and the South.
+    a. World War I
+    b. The American Revolution
+    c. The French and Indian War
+    d. The Civil War\n""")
+q6 = str("""Who did the United States fight in World War II?
+    a. England, Germany, and Russia
+    b. Japan, Germany, and Canada
+    c. Japan, Germany, and Italy
+    d. China, Japan, and Turkey\n""")
+q7 = str("""Before he was President, Eisenhower was a general. What war did he
+fight in?
+    a. World War II
+    b. The American Revolution
+    c. The Korean War
+    d. World War I\n""")
+q8 = str("""During the Cold War, what was the main concern of the United States?
+    a. Capitalism
+    b. Energy Shortage
+    c. Climate Change
+    d. Communism\n""")
+q9 = str("""Which is the state that borders Mexico?
+    a. Oklahoma
+    b. Louisiana
+    c. Mississippi
+    d. Arizona\n""")
+q0CorAns = str("a")
+q1CorAns = str("d")
+q2CorAns = str("d")
+q3CorAns = str("a")
+q4CorAns = str("b")
+q5CorAns = str("d")
+q6CorAns = str("c")
+q7CorAns = str("a")
+q8CorAns = str("d")
+q9CorAns = str("d")
+q0Ans = str("")
+q1Ans = str("")
+q2Ans = str("")
+q3Ans = str("")
+q4Ans = str("")
+q5Ans = str("")
+q6Ans = str("")
+q7Ans = str("")
+q8Ans = str("")
+q9Ans = str("")
+q0Ans = str("")
+check = bool(False)
